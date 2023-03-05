efc1c07e07189580abbc3bbbc4292679bdd17b44
Sven Günther <sven.guenther@tum.de>
2023-03-06 00:21:50 +0100

    Update get_data.m

--- get_data.m
+++ get_data.m
@@ -3,6 +3,6 @@ data_path = "sub"+i+"_data.csv";
 event_path = "sub"+i+"_event.csv";
 label_data = "sub"+i+"_event.csv";
 csvwrite(data_path, data{1,i}.trial{1,1});
-%writetable(struct2table(event{1,i}), "sub"+i+"_event.csv");
-%writetable(cell2table(data{1,i}.label), "sub"+i+"_label.csv");
+writetable(struct2table(event{1,i}), "sub"+i+"_event.csv");
+writetable(cell2table(data{1,i}.label), "sub"+i+"_label.csv");
 end
